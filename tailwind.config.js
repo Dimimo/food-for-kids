5c840e8ac1ebc9d53bf4bf6ba14b27b8401e2afd
Dimitri Mostrey <dmostrey@yahoo.com>
2020-09-26 20:35:45 +0800

    tailwind.config.js updated for future release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    future: {
+        purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+    },
+    purge: {
+        content: [
+            './vendor/laravel/jetstream/**/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+        ],
+    },
 
     theme: {
         extend: {
@@ -16,7 +22,10 @@ module.exports = {
     },
 
     variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+        opacity: [
+            'responsive', 'hover', 'focus'
+            /*, 'active', 'visited', 'checked', 'disabled', 'first', 'last', 'even', 'odd', 'group-hover', 'group-focus', 'focus-within'*/
+        ],
     },
 
     plugins: [require('@tailwindcss/ui')],
